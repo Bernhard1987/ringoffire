e9f7ab7e8526a44717513292d4ad34e2ba8a3560
Bernhard1987 <demoncleaner1@gmail.com>
2023-12-31 17:12:26 +0100

    added rule card and rules

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,17 +3,18 @@ import { Component } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { CardRulesComponent } from '../card-rules/card-rules.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatDialogModule],
+  imports: [CommonModule, PlayerComponent, CardRulesComponent, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -22,7 +23,7 @@ export class GameComponent {
   pickCardAnimation = false;
   currentCard: string = '';
   game: Game = new Game;
-  
+
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
